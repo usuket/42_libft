554f63b03195cb5775a464bdab33e37cd0145207
usuket <sy0202002@gmail.com>
2017-06-11 10:18:08 -0700

    Modify add.sh

--- add.sh
+++ add.sh
@@ -1,9 +1,24 @@
 #!/bin/zsh
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    add.sh                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ysakakib <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/06/11 10:08:43 by ysakakib          #+#    #+#              #
+#    Updated: 2017/06/11 10:08:51 by ysakakib         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 
 if [ $1 = "" ];then
     echo 'need 1 argv'
     exit 1;
 fi
+
+# TODO add 42 header automatically
+NAME=`cat author`
+DATE=`date "+%Y/%m/%d %H:%M:%S"`
 
 ADD_FILE=$1
 TARGET_FILE=Makefile
