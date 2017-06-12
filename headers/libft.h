7f55b5d67b8275727024093cc8842372c3b8c040
usuket <sy0202002@gmail.com>
2017-06-12 16:52:52 -0700

    Add ft_memcmp

--- headers/libft.h
+++ headers/libft.h
@@ -21,10 +21,9 @@ void 	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-// ft_memcmp
 size_t ft_strlen(const char *s);
-
 char	*ft_strdup(const char *s1);
 // ft_strcpy
 // ft_strncpy
---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysakakib <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/12 16:23:37 by ysakakib          #+#    #+#             */
+/*   Updated: 2017/06/12 16:23:39 by ysakakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int		i;
+	char	*str1;
+	char	*str2;
+
+	i = 0;
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
