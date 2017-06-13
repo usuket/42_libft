43a90e977199f63751d0a368ca27f6e0bc117444
usuket <sy0202002@gmail.com>
2017-06-13 14:53:47 -0700

    Add ft_memalloc.c

--- headers/libft.h
+++ headers/libft.h
@@ -47,7 +47,7 @@ int ft_toupper(int c);
 int ft_tolower(int c);
 
 // Additional Parts
-//void *ft_memalloc(size_t size);
+void *ft_memalloc(size_t size);
 //void ft_memdel(void **ap);
 //char *ft_strnew(size_t size);
 //void ft_strdel(char **as);
---
+++ srcs/ft_memalloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysakakib <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/13 14:37:35 by ysakakib          #+#    #+#             */
+/*   Updated: 2017/06/13 14:37:40 by ysakakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	mem = malloc(size);
+	if (mem == NULL)
+		return (NULL);
+	ft_memset(mem, 0, size);
+	return (mem);
+}
