4975f51f364d66bc2a304073d1d982bb80fe8fcd
usuket <sy0202002@gmail.com>
2017-06-13 14:18:58 -0700

    Add test_ft_putchar.c

---
+++ tests/test_ft_putchar.c
@@ -0,0 +1,27 @@
+// TODO
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libft.h"
+
+#define FUNCTION_NAME "ft_putchar"
+#define TARGET ft_putchar
+#define EXPECT putchar
+
+int main() {
+	printf("## TEST %s ##\n", FUNCTION_NAME);
+	printf("##########################\n");
+
+	for (int i = 0; i <= 127; ++i) {
+		TARGET(i);
+	}
+	TARGET('\n');
+
+	for (int i = 0; i <= 127; ++i) {
+		EXPECT(i);
+	}
+	EXPECT('\n');
+
+	return (0);
+}
