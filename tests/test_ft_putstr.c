3ad3f677970a62920fcdfcb9cce60d5a37bd1a3a
usuket <sy0202002@gmail.com>
2017-06-13 14:31:17 -0700

    Add test_ft_putstr.c

---
+++ tests/test_ft_putstr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libft.h"
+
+#define FUNCTION_NAME "ft_putstr"
+#define TARGET ft_putstr
+
+int main() {
+	printf("## TEST %s ##\n", FUNCTION_NAME);
+	printf("##########################\n");
+
+	TARGET("#putstr#qwertyuiop\t[]asdfghjkl;'zxcvbnm,./\n");
+	printf("#printf#qwertyuiop\t[]asdfghjkl;'zxcvbnm,./\n");
+
+	return (0);
+}
