c85697de56fabe62de620e76b2ca7524446ab620
usuket <sy0202002@gmail.com>
2017-06-14 12:06:29 -0700

    Mod test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 FILE_CHECKER_DIR="../42FileChecker/42FileChecker.sh"
 PROJECT_ROOT=`pwd`
-echo $FILE_CHECKER_DIR
-echo $PROJECT_ROOT
+echo "FILE_CHECKER_DIR = $FILE_CHECKER_DIR"
+echo "PROJECT_ROOT = $PROJECT_ROOT"
 
 bash $FILE_CHECKER_DIR --project "libft" --path $PROJECT_ROOT
