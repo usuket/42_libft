80cdd067476898c880a64c5c917edf6aa294869c
usuket <sy0202002@gmail.com>
2017-06-15 19:28:18 -0700

    ft_strsub.c

---
+++ srcs/ft_strsub.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysakakib <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/15 19:00:22 by ysakakib          #+#    #+#             */
+/*   Updated: 2017/06/15 19:00:23 by ysakakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str_len;
+
+	str_len = ft_memalloc(len);
+	if (str_len == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str_len[i] = s[start + i];
+		i++;
+	}
+	str_len[i] = '\0';
+	return (str_len);
+}
---
+++ tests/test_ft_strsub.c
@@ -0,0 +1,46 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+#define FUNCTION_NAME "ft_strsub"
+
+int main()
+{
+	printf("## TEST %s ##\n", FUNCTION_NAME);
+	printf("#######################\n");
+
+	char *str1 = "0123456789";
+	char *sub;
+
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			sub = ft_strsub(str1, i, j);
+			printf("%i:%i\t%s:%s\n", i, j, str1, sub);
+		}
+	}
+
+	printf("#######################\n");
+	sub = ft_strsub(str1, 0, 10);
+	printf("%i:%i\t%s:%s\n", 0, 10, str1, sub);
+
+	printf("## un expected ##\n");
+	sub = ft_strsub(str1, 20, 10);
+	printf("%i:%i\t%s:%s\n", 0, -10, str1, sub);
+
+	sub = ft_strsub(str1, 0, 20);
+	printf("%i:%i\t%s:%s\n", 0, 20, str1, sub);
+
+	sub = ft_strsub(str1, 0, -10);
+	printf("%i:%i\t%s:%s\n", 0, -10, str1, sub);
+
+	printf("## return null ##\n");
+	sub = ft_strsub(str1, 0, SIZE_MAX);
+	printf("%i:%llu\t%s:%s\t expect NULL\n", 0, SIZE_MAX, str1, sub);
+
+	return (0);
+}
