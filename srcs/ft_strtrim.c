4e33f8b3f396116773447b07b911686ff0ba66d0
usuket <sy0202002@gmail.com>
2017-06-15 21:29:42 -0700

    Add ft_strtrim

---
+++ srcs/ft_strtrim.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysakakib <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/15 20:16:07 by ysakakib          #+#    #+#             */
+/*   Updated: 2017/06/15 20:16:11 by ysakakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char			*ft_strtrim(char const *s)
+{
+	size_t		len;
+	int			i;
+	char		*str;
+	int			front;
+	int			end;
+
+	len = ft_strlen(s);
+	front = calc_front_trim_size(s, len);
+	end = calc_end_trim_size(s, len);
+	str = (char *)ft_memalloc(len - (front + end) + 1);
+	i = 0;
+	while (i < len - (front + end))
+	{
+		str[i] = s[front + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+static int		is_white_space(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t')
+		return (1);
+	return (0);
+}
+
+static int		calc_front_trim_size(const char *s, const int len)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (is_white_space(s[i]))
+			i++;
+		else
+			break ;
+	}
+	return (i);
+}
+
+static int		calc_end_trim_size(const char *s, const int len)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (is_white_space(s[len - i - 1]))
+			i++;
+		else
+			break ;
+	}
+	return (i);
+}
---
+++ tests/test_ft_strtrim.c
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+#define FUNCTION_NAME "ft_strtrim"
+
+int main()
+{
+	printf("## TEST %s ##\n", FUNCTION_NAME);
+	printf("#######################\n");
+
+	char *str1 = malloc(sizeof(char) * 10);
+	char *str2 = malloc(sizeof(char) * 10);
+
+	str1 = "0";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = " a ";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = " a b";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = " a b ";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = " a b c ";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	printf("## Multiple ##\n");
+
+	str1 = "  a b";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = "a b  ";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = "          a b        ";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	printf("## TAB and RETURN ##\n");
+
+	str1 = "\ta\tb\t";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = "\t\ta\t\tb\t\t";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = "  \t\ta\t\tb   ";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	str1 = "\na\tb\n";
+	printf("%s:%s$\n", str1, ft_strtrim(str1));
+
+	return (0);
+}
