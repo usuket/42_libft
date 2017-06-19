f4e03f61cd4f53fce79ea3915e2418189a6f0749
usuket <sy0202002@gmail.com>
2017-06-19 13:58:27 -0700

    WIP ft_strsplit.c

--- tests/test_ft_strsplit.c
+++ tests/test_ft_strsplit.c
@@ -7,16 +7,33 @@
 
 #define FUNCTION_NAME "ft_strsplit"
 
+static void print_list(char **list)
+{
+    int i = 0;
+    while (list[i]) {
+        char *str = list[i];
+        i++;
+        printf("%s$\n", str);
+    }
+}
+
 int main()
 {
 	printf("## TEST %s ##\n", FUNCTION_NAME);
 	printf("#######################\n");
 
-	char *str1 = malloc(sizeof(char) * 10);
-	char *str2 = malloc(sizeof(char) * 10);
+	char **list = ft_strsplit("aaa,bbb,ccc", ',');
+    print_list(list);
+    printf("\n");
+    list = ft_strsplit("0,,2,,4", ',');
+    print_list(list);
+    printf("#######################\n");
+    list = ft_strsplit("0,,2,,4", ',');
+    print_list(list);
+    printf("#######################\n");
 
-	str1 = "\na\tb\n";
-	printf("%s:%s$\n", str1, ft_strtrim(str1));
-
+    char **list2 = ft_strsplit("ccc", ',');
+    print_list(list2);
+    printf("\n");
 	return (0);
 }
