a5fde665799cf5d23c18f7d9224f5024be9ca25e
usuket <sy0202002@gmail.com>
2017-06-22 23:10:30 -0700

    Add ft_putchar_fd.c

--- headers/libft.h
+++ headers/libft.h
@@ -12,6 +12,8 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 // Mandatory parts
 void	*ft_memset(void *b, int c, size_t len);
@@ -64,7 +66,7 @@ void ft_putchar(char c);
 void ft_putstr(char const *s);
 void ft_putendl(char const *s);
 void ft_putnbr(int n);
-//void ft_putchar_fd(char c, int fd);
+void ft_putchar_fd(char c, int fd);
 //void ft_putstr_fd(char const *s, int fd);
 //void ft_putendl_fd(char const *s, int fd);
 //void ft_putnbr_fd(int n, int fd);
---
+++ tests/test_ft_putchar_fd.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+#define FUNCTION_NAME "ft_putchar_fd"
+
+int main() {
+	int fd;
+	int fd1;
+
+	char *path = "output/test_ft_putchar_fd.log";
+	fd = open(path, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	ft_putchar_fd('x', fd);
+	close(fd);
+
+	fd1 = open(path, O_RDONLY);
+	char t = 0;
+	while (read(fd1, &t, 1) > 0) {
+		printf("%c", t);
+	}
+	close(fd1);
+	return (0);
+}
