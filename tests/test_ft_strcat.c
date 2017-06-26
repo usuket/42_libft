83c54dcf57a376507e9b801786f8b79bb2d5e808
usuket <sy0202002@gmail.com>
2017-06-25 17:52:27 -0700

    ft_strcat

---
+++ tests/test_ft_strcat.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+#define FUNCTION_NAME "ft_strcat"
+
+int main() {
+	printf("## TEST %s ##\n", FUNCTION_NAME);
+	printf("##########################\n");
+
+	char str1[30] = "ABC";
+
+
+	strcat(str1, "xyz");
+	printf("%s\n", str1);
+
+	char str2[30] = "ABC";
+	ft_strcat(str2, "xyz");
+	printf("%s\n", str2);
+
+	return 0;
+}
