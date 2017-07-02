28d1e3c90d241e07b29f4c57e87146fe430dd4d5
usuket <sy0202002@gmail.com>
2017-07-02 01:45:49 -0700

    protect malloc return

--- srcs/ft_strmap.c
+++ srcs/ft_strmap.c
@@ -20,6 +20,8 @@ char	*ft_strmap(char const *s, char (*f)(char))
 
 	len = ft_strlen(s);
 	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
 	i = 0;
 	while (i < len && *s)
 	{
