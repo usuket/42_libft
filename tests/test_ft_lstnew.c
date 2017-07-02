3d30fd10d088da3ddf71bf19e885c5a9e5b2515c
usuket <sy0202002@gmail.com>
2017-07-02 16:56:18 -0700

    test new list

--- tests/test_ft_lstnew.c
+++ tests/test_ft_lstnew.c
@@ -3,6 +3,16 @@
 
 #include "libft.h"
 
+#define FUNCTION_NAME "ft_lstnew"
+
 int main(void) {
+	printf("## TEST %s ##\n", FUNCTION_NAME);
+	printf("##########################\n");
+
+	t_list *list = ft_lstnew("ABC", 3);
+	printf("%s\n", list->content);
+
+	t_list *list_null = ft_lstnew(NULL, 3);
+	printf("%s\n", list_null->content);
 	return (0);
 }
