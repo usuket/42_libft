dd7023344e4009bedb6e7f222949011e966017d3
usuket <sy0202002@gmail.com>
2017-07-02 01:05:27 -0700

    Fix

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -12,28 +12,25 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	number;
+	unsigned int	i;
+	long long	number;
 	int	negative_flag;
 
 	number = 0;
-	negative_flag = 0;
+	negative_flag = 1;
 	i = 0;
 	while ((str[i] == '\n') || (str[i] == '\t') || (str[i] == '\v') ||
 		   (str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
 		i++;
 	if (str[i] == '-')
-		negative_flag = 1;
+		negative_flag = -1;
 	if (str[i] == '+' || str[i] == '-')
 		i++;
 	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
 	{
 		number *= 10;
-		number += (int)str[i] - '0';
+		number += (long)str[i] - '0';
 		i++;
 	}
-	if (negative_flag == 1)
-		return (-number);
-	else
-		return (number);
+	return (number * negative_flag);
 }
